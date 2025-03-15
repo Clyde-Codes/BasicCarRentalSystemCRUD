01936a5f6b033d8b6759dc34ba2ab64731f0844b
clyde laurence lacap <T22-35046@mseuf.student.edu.ph>
2025-03-15 21:06:08 +0800

    Add project files.

---
+++ BasicCarRentalSystemCRUD/Models/CarRenter.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicCarRentalSystemCRUD.Models
+{
+    public class CarRenter
+    {
+        [Required]
+        public string? CarModel { get; set; }
+        [Required]
+        public string? CustomerName { get; set; }
+
+        [Required]
+        public DateTime RentalDate { get; set; }
+        [Required]
+        public DateTime ReturnDate { get; set; }
+    }
+}
